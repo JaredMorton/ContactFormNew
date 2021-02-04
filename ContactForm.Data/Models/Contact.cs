6a473004f5ea0a150a5ee952a1a8b7c05970dea8
jm829 <jm829@LT431>
2021-02-04 17:42:11 -0500

    Seperating app into multiple projects

---
+++ ContactForm.Data/Models/Contact.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContactForm.Data.Models
+{
+    public class Contact
+    {
+        [Key]
+        public int ContactID { get; set; }
+
+        [Description("First Name")]
+        public string FirstName { get; set; }
+
+        [Description("Last Name")]
+        public string LastName { get; set; }
+
+        public Address Address { get; set; }
+
+    }
+}
